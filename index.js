b4855b528ec83c13d75765787b9faf3074d6d37b
Douglas W Schultz <vxpx17@gmail.com>
2017-11-25 23:13:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,8 @@ function logShout(string){
 function logWhisper(string){
   console.log(whisper(string));}
 
+function sayHiToGrandma(string){
+  if (string.toLowerCase()===true){return "I can't hear you!"}
+  else if (string.toUpperCase()===true){return "YES INDEED!"}
+  else if(string==="I love you, Grandma."){return "I love you,too."}}
+}
