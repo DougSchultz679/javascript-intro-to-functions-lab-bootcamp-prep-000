16e3aaa3a135b1b039cbf5bd3cefd2373ec69696
Douglas W Schultz <vxpx17@gmail.com>
2017-11-25 23:22:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,3 +16,4 @@ function sayHiToGrandma(string){
   else if (string===uppercase){return 'YES INDEED!';}
   else if (string==="I love you, Grandma."){return "I love you, too.";}}
 console.log(sayHiToGrandma('hello!'));
+console.log(sayHiToGrandma('HELLO!'));
